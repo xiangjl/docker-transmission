cf64aac9464545d6ce006c7bf80199b0be1c82f9
xiangjl <xjl-tommy@qq.com>
2016-01-15 12:45:58 +0800

    fix:修正配置文件包路径

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -d '/var/lib/transmission/.config/' ]; then
-	tar zxvf /root/config.tar.gz
+	tar zxvf /docker/config.tar.gz
 fi
 
 /usr/bin/transmission-daemon --foreground --config-dir '/var/lib/transmission/.config/transmission-daemon/' --log-error
