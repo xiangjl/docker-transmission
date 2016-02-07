daedb71ee1e1f64fb45cf58b2bf64561121df6d7
xiangjl <xjl-tommy@qq.com>
2016-02-07 09:42:12 +0800

    bugfix:修改启动参数

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ if [ ! -d '/var/lib/transmission/.config/' ]; then
 	tar zxvf /docker/config.tar.gz -C /var/lib/transmission/
 fi
 
-/usr/bin/transmission-daemon --foreground --log-error
+/usr/bin/transmission-daemon --foreground --log-error --config-dir /var/lib/transmission/
